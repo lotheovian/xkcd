014120075fa3519b64dd4ee54e5042b61a21e82d
Zachary <26127076+lotheovian@users.noreply.github.com>
2018-11-15 11:04:00 -0500

    Update and rename dl.sh to xkcd_mailer.sh

--- xkcd_mailer.sh
+++ xkcd_mailer.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-FROM_EMAIL="emailToSend@whatever.com"
-MAILING_LIST="mailinglist@whatever.com,otheremail@whatever.com"
 
 function getComic() {
    NUM_TO_GET=$1
@@ -38,6 +36,21 @@ COMIC_URLS_FETCHED=""
 COMICS_FETCHED=0
 
 XKCD_DIR="$(echo ~/Documents/xkcd/)"
+XKCD_CONFIG="~/Docuemnts/xkcd_config"
+if [ ! -f $XKCD_CONFIG ]
+then echo please make a config file with a FROM_EMAIL and MAILING_LIST in the file $XKCD_CONFIG
+     exit 1
+fi
+source $XKCD_CONFIG
+if [ -z "$FROM_EMAIL" ]
+then echo please specific a FROM_EMAIL in $XKCD_CONFIG
+     exit 1
+fi
+if [ -z "$MAILING_LIST" ]
+then echo please specific a MAILING_LIST in $XKCD_CONFIG
+     exit 1
+fi
+
 mkdir -p ${XKCD_DIR}
 COMIC_COUNT="$(find ${XKCD_DIR} -maxdepth 1 -type f|wc -l|tr -d ' ')"
 CURRENT_COMIC="$(curl https://xkcd.com 2> /dev/null | sed -e '/Permanent link to this comic:/!d;s/.*https\{0,1\}:\/\/xkcd.com\/\([0-9]*\)\/.*/\1/')"
@@ -45,10 +58,11 @@ printf "You currently have %s/%s (%s%%) xkcd comics.\n" ${COMIC_COUNT} ${CURRENT
 
 #Fetch new Comics
 for COMIC_NUM in $(seq 1 ${CURRENT_COMIC})
-do
-   #Excludes
+do #Excludes
    isExcluded ${COMIC_NUM} && continue
-   compgen -G "${XKCD_DIR}/${COMIC_NUM}_*" &> /dev/null || getComic ${COMIC_NUM}
+   if ! compgen -G "${XKCD_DIR}/${COMIC_NUM}_*" &> /dev/null
+   then getComic ${COMIC_NUM}
+   fi
 done
 
 #Send out our daily lolz email
@@ -56,20 +70,18 @@ TMP="$(mktemp /tmp/XXXXX)"
 makeMailHeader > ${TMP}
 
 if [ -n "${COMICS_FETCHED}" ] # Send out the latest stuff
-then
-   for COMIC in ${COMIC_URLS_FETCHED}
-   do
-      printf "<br><p>Link to comic %s</p>" ${COMIC} >> $TMP
-      printf "<img src=\"%s\">" ${COMIC} >> $TMP
-   done
+then for COMIC in ${COMIC_URLS_FETCHED}
+     do printf "<br><p>Link to comic %s</p>" ${COMIC} >> $TMP
+        printf "<img src=\"%s\">" ${COMIC} >> $TMP
+     done
 else # Or pick 5 random comics   
-   for RAND_COMIC in $(shut -i 1-${CURRENT_COMIC} -n 5)
-   do
-      isExcluded ${RAND_COMIC} && continue
-      RAND_COMIC_URL=$(curl https://xkcd.com/${RAND_COMIC}/ 2> /dev/null | grep "Image URL (for hotlinking/embedding):" | sed "s/.*embedding):\(.*\)/\1/")
-      printf "<br><p>Link to comic %s</p>\n" "https://xkcd.com/${RAND_COMIC}/" >> $TMP
-      printf "<img src=\"%s\">\n" ${RAND_COMIC_URL} >> $TMP
-   done
+     for RAND_COMIC in $(shut -i 1-${CURRENT_COMIC} -n 5)
+     do isExcluded ${RAND_COMIC} && continue
+        RAND_COMIC_URL=$(curl https://xkcd.com/${RAND_COMIC}/ 2> /dev/null | grep "Image URL (for hotlinking/embedding):" | sed "s/.*embedding):\(.*\)/\1/")
+        printf "<br>Click <a href=\"%s\">here</a> for the comic.\n" "https://xkcd.com/${RAND_COMIC}/" >> $TMP
+        printf "<br>Click <a href=\"%s\">here</a> for the explanation.\n" "https://www.explainxkcd.com/wiki/index.php/${RAND_COMIC}" >> $TMP
+        printf "<img src=\"%s\">\n" ${RAND_COMIC_URL} >> $TMP
+     done
 fi
 printf "</body>\n</html>\n" >> $TMP
-cat $TMP|sendmail "${MAILING_LIST}" # Send it
+cat $TMP | sendmail "${MAILING_LIST}" # Send it
